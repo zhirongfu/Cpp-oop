3b10a11b901df76e7b3c99ce3e28afeaec5ee7bf
Levi <ZHI.FU57@myhunter.cuny.edu>
2023-02-02 02:02:14 -0500

    commit

--- Book.cpp
+++ Book.cpp
@@ -1,56 +1,56 @@
 #include <iostream>
 #include "Book.hpp"
-Book::Book()
+Book::Book()//default constructor
 {
-    pagecount=0;
-    available=false;
+    pagecount=0;//default pagecount is 0
+    available=false;//default digital availability is false
 }
-Book::Book(std::string title1,std::string author1,int pagecount1)
+Book::Book(std::string title1,std::string author1,int pagecount1)//constructor if they don't provide digital availability
 {
     title=title1;
     author=author1;
     pagecount=pagecount1;
     available=false;
 }
-Book::Book(std::string title1,std::string author1,int pagecount1, bool available1)
+Book::Book(std::string title1,std::string author1,int pagecount1, bool available1)//constructor if everything is provided
 {
     title=title1;
     author=author1;
     pagecount=pagecount1;
     available=available1;    
 }
-void Book::setTitle(std::string title1)
+void Book::setTitle(std::string title1)//sets title
 {
     title=title1;
 }
-std::string Book::getTitle()
+std::string Book::getTitle()const//returns title
 {
     return title;
 }
-void Book::setAuthor(std::string author1)
+void Book::setAuthor(std::string author1)//sets author of the book
 {
     author=author1;
 }
-std::string Book::getAuthor()
+std::string Book::getAuthor()const//returns author
 {
     return author;
 }
-void Book::setPageCount(int pagecount1)
+void Book::setPageCount(int pagecount1)//sets page count
 {
-    if(pagecount1>0)
+    if(pagecount1>0)//only allows positive pagecount
     {
         pagecount=pagecount1;
     }
 }
-int Book::getPageCount()
+int Book::getPageCount()const//returns pagecount
 {
     return pagecount;
 }
-void Book::setDigital()
+void Book::setDigital()//sets digital availablity to true
 {
     available=true;
 }
-bool Book::isDigital()
+bool Book::isDigital()const//returns availability
 {
     return available;
 }
--- Book.hpp
+++ Book.hpp
@@ -4,22 +4,22 @@
 #include <iostream>
 class Book
 {
-private:
-    std::string title;
-    std::string author;
-    int pagecount;
-    bool available;
-public:
-    Book();
-    Book(std::string title1,std::string author1,int pagecount1, bool available1);
-    Book(std::string title1,std::string author1,int pagecount1);
-    void setTitle(std::string);
-    std::string getTitle();
-    void setAuthor(std::string);
-    std::string getAuthor();
-    void setPageCount(int);
-    int getPageCount();
-    void setDigital();
-    bool isDigital();
+private://members
+    std::string title;//title of book       
+    std::string author;//author of book
+    int pagecount;//pagecount of book
+    bool available;//digital availability of book
+public://member functions
+    Book();//default constructor
+    Book(std::string title1,std::string author1,int pagecount1, bool available1);//constructor with all 4 mem
+    Book(std::string title1,std::string author1,int pagecount1);//constructor with 3 
+    void setTitle(std::string);//setter
+    std::string getTitle()const;//getter
+    void setAuthor(std::string);//setter
+    std::string getAuthor()const;//getter
+    void setPageCount(int);//setter
+    int getPageCount()const;//getter
+    void setDigital();//setter
+    bool isDigital()const;//getter
 };
 #endif
